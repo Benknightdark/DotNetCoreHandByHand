07bb6029868dc54b9b740bf2e9cadf935d6dfb77
moon791017 <moon791017@gmail.com>
2019-11-05 12:05:41 +0800

    add db context

---
+++ Models/DBModels/TestTable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DotnetCoreHandByHand.Models.DBModels
+{
+    public partial class TestTable
+    {
+        public Guid ID { get; set; }
+        public string ColumnName1 { get; set; }
+        public string ColumnName2 { get; set; }
+    }
+}
---
+++ Models/DBModels/TutorialContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace DotnetCoreHandByHand.Models.DBModels
+{
+    public partial class TutorialContext : DbContext
+    {
+        public TutorialContext()
+        {
+        }
+
+        public TutorialContext(DbContextOptions<TutorialContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<TestTable> TestTable { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.0-rtm-35687");
+
+            modelBuilder.Entity<TestTable>(entity =>
+            {
+                entity.Property(e => e.ID).ValueGeneratedNever();
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
